be0cf43fbb0dec421b9419744dbf58a74d61672b
Atsunobu Yuge <yugechan666@gmail.com>
2021-11-11 13:15:53 +0900

    Initial Commit

---
+++ nobuKadai1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nobuKadai1
+//
+//  Created by 弓削　敦信 on 2021/11/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nobuKadai1/nobuKadai1App.swift
@@ -0,0 +1,17 @@
+//
+//  nobuKadai1App.swift
+//  nobuKadai1
+//
+//  Created by 弓削　敦信 on 2021/11/11.
+//
+
+import SwiftUI
+
+@main
+struct nobuKadai1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
